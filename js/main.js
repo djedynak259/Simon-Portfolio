c3558dae2c9dd8c5ae9e99ee44cf7dc8ed7062e6
djedynak259 <dajed@outlook.com>
2016-09-19 23:15:51 -0700

    9/19 cmt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-test
+$(document).ready(function() {
+    $(".navbar a").click(function() {
+       var id = $(this).attr("data-id");
+       $("#content > div").hide(); 
+       $(".div" + id).show(); 
+    });
+    $(".navbar li").click(function() {
+    	$(".navbar li").removeClass('active');
+    	$(this).addClass('active');
+    });
+
+});
